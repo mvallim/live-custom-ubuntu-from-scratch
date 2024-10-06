44b0aa3ed4e1855e28d040f8b4b29034a0c5b0b1
Gleb Orlioglo <glebvmx@gmail.com>
2024-10-06 21:31:26 +0300

    scripts/default_config.sh upd.

--- scripts/default_config.sh
+++ scripts/default_config.sh
@@ -7,11 +7,11 @@
 
 # The version of Ubuntu to generate.  Successfully tested LTS: bionic, focal, jammy, noble
 # See https://wiki.ubuntu.com/DevelopmentCodeNames for details
-export TARGET_UBUNTU_VERSION="focal"
+export TARGET_UBUNTU_VERSION="noble"
 
 # The Ubuntu Mirror URL. It's better to change for faster download.
 # More mirrors see: https://launchpad.net/ubuntu/+archivemirrors
-export TARGET_UBUNTU_MIRROR="http://us.archive.ubuntu.com/ubuntu/"
+export TARGET_UBUNTU_MIRROR="https://mirror.yandex.ru/ubuntu/"
 
 # The packaged version of the Linux kernel to install on target image.
 # See https://wiki.ubuntu.com/Kernel/LTSEnablementStack for details
@@ -19,13 +19,13 @@ export TARGET_KERNEL_PACKAGE="linux-generic"
 
 # The file (no extension) of the ISO containing the generated disk image,
 # the volume id, and the hostname of the live environment are set from this name.
-export TARGET_NAME="ubuntu-from-scratch"
+export TARGET_NAME="Rubuntu"
 
 # The text label shown in GRUB for booting into the live environment
-export GRUB_LIVEBOOT_LABEL="Try Ubuntu FS without installing"
+export GRUB_LIVEBOOT_LABEL="Try Rubuntu without installing"
 
 # The text label shown in GRUB for starting installation
-export GRUB_INSTALL_LABEL="Install Ubuntu FS"
+export GRUB_INSTALL_LABEL="Install Rubuntu"
 
 # Packages to be removed from the target system after installation completes succesfully
 export TARGET_PACKAGE_REMOVE="
@@ -53,8 +53,13 @@ function customize_image() {
         curl \
         vim \
         nano \
-        less
-
+        less \
+        git
+    #set plymouth theme to angular
+    cd /usr/share/plymouth/themes/
+    git clone https://github.com/glebvmx/angular
+    rm /etc/alternatives/default.plymouth
+    ln -s /usr/share/plymouth/themes/angular/angular.plymouth /etc/alternatives/default.plymouth 
     # purge
     apt-get purge -y \
         transmission-gtk \
